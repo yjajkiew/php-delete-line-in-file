f9994a0ef7b043ac5d8ac85f37449432a646c510
Yann Jajkiewicz <yann.jajkiewicz@gmail.com>
2012-08-02 13:13:07 +0300

    Update php-delete-line-in-file.php

--- php-delete-line-in-file.php
+++ php-delete-line-in-file.php
@@ -2,29 +2,18 @@
 	
 function deleteLineInFile($file,$string)
 {
-	$i=0; $j=0; $array=array();
+	$i=0;$j=0;$array=array();
 	
 	$read = fopen($file, "r") or die("can't open the file");
-	
-	while ($data = fgetcsv($read, 1024, ';')) {
-		foreach($data as $d) 
-		{
-			if(!$j) $array[$i] .= $d;
-			else $array[$i] .= ';' . $d;
-			++$j;
-		}
-		$j=0;++$i;
+	while(!feof($read)) {
+		$array[$i] = fgets($read);	
+		++$i;
 	}
 	fclose($read);
 	
 	$write = fopen($file, "w") or die("can't open the file");
-	
 	foreach($array as $a) {
-		if(!strstr($a,$string))
-		{
-			fwrite($write,$a);
-			fwrite($write,PHP_EOL);
-		}
+		if(!strstr($a,$string)) fwrite($write,$a);
 	}
 	fclose($write);
 }
